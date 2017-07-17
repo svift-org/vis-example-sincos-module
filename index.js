0438d93dc95dfe0e189a66b7e2540d543c1079d1
sebastian-meier <contact@sebastianmeier.eu>
2017-07-17 20:43:25 +0200

    initial sincos example

---
+++ index.js
@@ -0,0 +1,37 @@
+SVIFT.vis.example.sincos = (function (data, container) {
+ 
+  // Module object
+  var module = SVIFT.vis.base(data, container);
+ 
+  module.setup = function () {
+    module.g.append('circle')
+      .attr('r', 5)
+      .attr('cy', 20)
+      .attr('cx', 0)
+      .style('stroke','#000')
+      .style('fill','#000');
+  };
+
+  module.resize = function () {
+    var width = module.container.node().offsetWidth,
+      height = module.container.node().offsetHeight;
+
+    module.timeline.circle.obj.interpolate = d3.interpolate(20, height-20);
+
+    if(!module.playState){
+      module.draw(module.playHead);
+    }
+  };
+
+  module.drawCircle = function(t){
+    module.g.select('circle').attr('y', module.timeline.circle.obj.interpolate(
+      (Math.sin( (t * Math.PI*20) - (Math.PI/2) ) + 1)/2
+    ));
+  };
+
+  module.timeline = {
+    circle: {start:0, end:10000, func:module.drawCircle, obj:{interpolate:null}}
+  };
+
+  return module;
+ });
